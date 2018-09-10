607066519f2e1039a01da6325e7e303508ea78c0
Ma,Ning(nima) <nima@ebay.com>
2018-09-09 18:22:07 -0700

    install required package by setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='data_analysis',
+    version='0.0.1',
+    url='https://github.com/mmaning/data-analysis',
+    author='Tony Ma',
+    packages=find_packages(),
+    install_requires=['PyQt5', 'pandas', 'sqlalchemy', 'nltk', 'numpy', 'jupyter', 'python-twitter'],
+    entry_points={},
+    extras_require={'dev': ['flake8',]},
+)
